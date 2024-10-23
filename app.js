b2c407571516490cea3bf1214563db4e46e98a07
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2024-10-23 14:25:13 -0400

    api start

--- app.js
+++ app.js
@@ -5,4 +5,16 @@ function handleSearchSubmit(e){
     var searchInput = document.querySelector("#search-form-input");
     var cityElement = document.querySelector("#selectedCity");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
+}
+
+function searchCity(city){
+    var apiKey = "5oa4a52efabffa028f08et03bf538250";
+    var apiURL = "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+    axios.get(apiURL).then(updateWeatherInfo);
+}
+
+function updateWeatherInfo(response){
+    console.log(response.data);
+    var temperatureElement = 
 }
