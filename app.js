66e9cc75718f5bcf2a039b5dddf85282c13fec2f
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2024-10-24 14:00:47 -0400

    enter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
 function handleSearchSubmit(e){
     e.preventDefault();
     var searchInput = document.querySelector("#search-form-input");
