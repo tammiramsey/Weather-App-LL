01cc3c8e51dc109ab19489762d4cc78795331337
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2024-10-24 13:49:50 -0400

    api

--- app.js
+++ app.js
@@ -16,5 +16,9 @@ function searchCity(city){
 
 function updateWeatherInfo(response){
     console.log(response.data);
-    var temperatureElement = 
+    var temperatureElement = document.querySelector(#temperature);
+    var temperature = response.data.temperature.current;
+    var cityElement = document.querySelector(#city);
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
 }
