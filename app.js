7342bc75d0ec1e94b81280fd4f2d2537cb5fe177
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2024-11-06 14:03:31 -0500

    event

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-function handleSearchSubmit(e){
-    e.preventDefault();
+function handleSearchSubmit(event){
+    event.preventDefault();
     var searchInput = document.querySelector("#search-form-input");
     searchCity(searchInput.value);
 }
