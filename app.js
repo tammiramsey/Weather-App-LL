44525ac765b2f8ea52a17277cf4a00bed45fe9dc
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2025-02-19 13:22:09 -0500

    restore original code

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 function searchCity(city){
     var apiKey = "a3o950fc274379347b6a44aft08a3cb0";
     var apiURL = `https://api.shecodes.io/weather/v1/current?query=${query}&key${apiKey}&units=metric`;
-    console.log(apiURL);
+    axios.get(apiURL).then(updateWeatherInfo);
 }
 function handleSearchSubmit(event){
     event.preventDefault();
     var searchInput = document.querySelector("#search-form-input");
-    var citySelector
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
+function updateWeatherInfo(response){
+    var temperatureElement = document.querySelector("#temperature");
+    var temperature = response.data.temperature.current;
+    var cityElement = document.querySelector("#selected-city");
+    var descriptionElement = document.querySelector("#description");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    capitalize(description);
+    console.log(response.data.condition.description);
+}
+function capitalize(description){
+    var description = response.data.condition.description;
+    description = description.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+    descriptionElement.innerHTML = description;
+}
+var searchFormElement = docum.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("London");
