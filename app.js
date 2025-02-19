5d000548060d3177e06d28740957a2f7feb91bff
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2025-02-19 13:27:57 -0500

    domloaded

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-function searchCity(city){
-    let query = document.getElementById('searchInput').value;
-    var apiKey = "a3o950fc274379347b6a44aft08a3cb0";
-    var apiURL = `https://api.shecodes.io/weather/v1/current?query=${query}&key${apiKey}&units=metric`;
-    axios.get(apiURL).then(updateWeatherInfo);
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+    function searchCity() {
+        let query = document.getElementById('searchInput').value;
+        var apiKey = "a3o950fc274379347b6a44aft08a3cb0";
+        var apiURL = `https://api.shecodes.io/weather/v1/current?query=${query}&key${apiKey}&units=metric`;
+        axios.get(apiURL).then(updateWeatherInfo);
+    }
+    // Other code...
+});
+
 function handleSearchSubmit(event){
     event.preventDefault();
     var searchInput = document.querySelector("#search-form-input");
