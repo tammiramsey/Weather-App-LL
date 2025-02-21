5ad94bc1e9e1acd7eb2d81aa17980fbccfa81f83
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2025-02-21 13:35:13 -0500

    add to uwi

--- app.js
+++ app.js
@@ -10,20 +10,10 @@ function updateWeatherInfo(response){
     var iconElement = document.querySelector("#icon");
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
-    capitalize(description);
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windElement.innerHTML = `${response.data.temperature.wind} km/h`;
-    timeElement.innerHTML = formatDate(date);
-    iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon"/>`;
-    console.log(response.data.condition.description);
-}
-function capitalize(description){
     var description = response.data.condition.description;
     var descriptionElement = document.querySelector("#description");
     description = description.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
     descriptionElement.innerHTML = description;
-}
-function formatDate(date){
     var date = new Date (response.data.time * 1000);
     var day = days[date.getDay()];
     var hours = date.getHours();
@@ -33,7 +23,13 @@ function formatDate(date){
         minutes = `0${minutes}`;
     }
     return `${day}, ${hours}:${minutes}`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.temperature.wind} km/h`;
+    timeElement.innerHTML = formatDate(date);
+    iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+    console.log(response.data.condition.description);
 }
+
 function searchCity(city){
     let query = document.getElementById('searchInput').value;
     var apiKey = "a3o950fc274379347b6a44aft08a3cb0";
