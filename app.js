1f234637f3437d7a1fd07ff4b62a5f93fca08ddf
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2025-02-21 13:14:25 -0500

    fix entire

--- app.js
+++ app.js
@@ -16,9 +16,17 @@ function updateWeatherInfo(response){
     var temperature = response.data.temperature.current;
     var cityElement = document.querySelector("#selected-city");
     var descriptionElement = document.querySelector("#description");
+    var humidityElement = document.querySelector("#humid");
+    var windElement = document.querySelector("#wind");
+    var timeElement = document.querySelector("#time");
+    var iconElement = document.querySelector("#icon");
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
     capitalize(description);
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.temperature.wind} km/h`;
+    timeElement.innerHTML = formatDate(date);
+    iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon"/>`;
     console.log(response.data.condition.description);
 }
 function capitalize(description){
@@ -26,6 +34,17 @@ function capitalize(description){
     var descriptionElement = document.querySelector("#description");
     description = description.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
     descriptionElement.innerHTML = description;
+}
+function formatDate(date){
+    var date = new Date (response.data.time * 1000);
+    var day = days[date.getDay()];
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    if(minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    return `${day}, ${hours}:${minutes}`;
 }
 var searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
