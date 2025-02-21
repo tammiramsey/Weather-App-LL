d3e6ff7f6438799dbfc46ca16eae3aed8f1038f8
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2025-02-21 13:31:20 -0500

    placeholders

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-function searchCity(city){
-    let query = document.getElementById('searchInput').value;
-    var apiKey = "a3o950fc274379347b6a44aft08a3cb0";
-    var apiURL = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
-    axios.get(apiURL).then(updateWeatherInfo);
-}
 
-function handleSearchSubmit(event){
-    event.preventDefault();
-    var searchInput = document.querySelector("#search-form-input");
-    cityElement.innerHTML = searchInput.value;
-    searchCity(searchInput.value);
-}
 function updateWeatherInfo(response){
     var temperatureElement = document.querySelector("#temperature");
     var temperature = response.data.temperature.current;
@@ -46,6 +34,20 @@ function formatDate(date){
     }
     return `${day}, ${hours}:${minutes}`;
 }
+function searchCity(city){
+    let query = document.getElementById('searchInput').value;
+    var apiKey = "a3o950fc274379347b6a44aft08a3cb0";
+    var apiURL = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(updateWeatherInfo);
+}
+
+function handleSearchSubmit(event){
+    event.preventDefault();
+    var searchInput = document.querySelector("#search-form-input");
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
+}
+
 var searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("London");
