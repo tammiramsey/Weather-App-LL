b4a23734116c45c34a0aa1feefe183c0366bded8
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2025-02-24 09:20:32 -0500

    rearrange

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 function updateWeatherInfo(response){
     var temperatureElement = document.querySelector("#temperature");
     var temperature = response.data.temperature.current;
-    var cityElement = document.querySelector("#selected-city");
-    var descriptionElement = document.querySelector("#description");
-    var humidityElement = document.querySelector("#humid");
-    var windElement = document.querySelector("#wind");
-    var timeElement = document.querySelector("#time");
-    var iconElement = document.querySelector("#icon");
-    cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
+    var cityElement = document.querySelector("#selected-city");
+    cityElement.innerHTML = response.data.city;
+    var descriptionElement = document.querySelector("#description");
     var description = response.data.condition.description;
     var descriptionElement = document.querySelector("#description");
     description = description.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
     descriptionElement.innerHTML = description;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windElement.innerHTML = `${response.data.temperature.wind} km/h`;
-    timeElement.innerHTML = formatDate(date);
     console.log(description);
+    var humidityElement = document.querySelector("#humid");
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    var windElement = document.querySelector("#wind");
+    windElement.innerHTML = `${response.data.temperature.wind} km/h`;
+    var iconElement = document.querySelector("#icon");
     iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+    var timeElement = document.querySelector("#time");
     var date = new Date (response.data.time * 1000);
+    timeElement.innerHTML = formatDate(date);
     var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     var day = days[date.getDay()];
     var hours = date.getHours();
@@ -28,6 +28,7 @@ function updateWeatherInfo(response){
     }
     return `${day}, ${hours}:${minutes}`;
 }
+    
 
 function searchCity(city){
     var apiKey = "a3o950fc274379347b6a44aft08a3cb0";
