ab1b7aa59b01d46ef9fd1ffe8f65c2b66d8c3e71
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2025-02-26 13:10:08 -0500

    forecast api

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function updateWeatherInfo(response){
     description = description.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
     descriptionElement.innerHTML = description;
     console.log(description);
+    getForecast(response.data.city);
     var humidityElement = document.querySelector("#humid");
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     var windElement = document.querySelector("#wind");
@@ -28,12 +29,38 @@ function updateWeatherInfo(response){
     }
     return `${day}, ${hours}:${minutes}`;
 }
-    
+
+
+function displayForecast(response){
+    var forecast = document.querySelector("#forecast")
+    var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    var forecastHTML ="";
+    days.forEach(function(day){
+        forecastHTML = 
+            `<div class="WeatherForecastPreview">
+                <div class="forecast-time">Tue</div>
+                    <div id="icon"></div>
+                    <canvas width="38" height="38"></canvas>
+                    <div class="forecast-temperature">
+                        <span class="forecast-icon"></span>
+                        <span class="forecast-temperature-max">23°C</span>
+                        <span class="forecast-temperature-min">14°C</span>
+                    </div>
+            </div>`;
+    });
+    forecast.innerHTML = forecastHTML;
+}
 
 function searchCity(city){
     var apiKey = "a3o950fc274379347b6a44aft08a3cb0";
     var apiURL = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
     axios.get(apiURL).then(updateWeatherInfo);
+}
+
+function getForecast(city){
+    var apiKey = "a3o950fc274379347b6a44aft08a3cb0";
+    var apiURL = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(displayForecast);
 }
 
 function handleSearchSubmit(event){
@@ -46,3 +73,4 @@ function handleSearchSubmit(event){
 var searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit());
 searchCity("London");
+displayForecast("London");
